be9c32338d38a7abe5d1fcba22789003f64dafbb
Hammam <mohammedhmmam@gmail.com>
2021-12-04 20:07:50 +0200

    Generate Nav menu Items

--- js/app.js
+++ js/app.js
@@ -1,63 +1,31 @@
 
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+///////////////////////////////////////////////////
+/////// Create Nav Menu Elemetns Start Here/////////////////
+///////////////////////////////////////////////////
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
+//Query the All section
+const sections = document.querySelectorAll('section');
 
-/**
- * Define Global Variables
- * 
-*/
+//Create Fragment that will be contain elements
+const navFragment = document.createDocumentFragment();
+
+//Ul Element
+const navUl = document.getElementById('navbar__list');
 
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+//Loop on each section to get the data
+for(section of sections){
+    const liItem = document.createElement('li');
+    const liLink = document.createElement('a');
+    liLink.setAttribute('href' , '#' + section.getAttribute('id'));
+    liLink.textContent = section.getAttribute('id');
+    
+    liItem.appendChild(liLink);
 
+    navFragment.appendChild(liItem);
+}
 
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+navUl.appendChild(navFragment);
+///////////////////////////////////////////////////
+/////// Create Nav Menu Elemetns End Here/////////////////
+///////////////////////////////////////////////////
